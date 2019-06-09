aaf493467bf18999c4e4e581fbd82341dbea09ad
Santhosh Kumar Rajamanickam <santhosh.rajamanickam@student.uva.nl>
2019-06-09 13:36:12 +0200

    Update with final wikipedia processing changes

--- content_processing.py
+++ content_processing.py
@@ -1,64 +1,236 @@
+import re
 import nltk
 import spotlight
-from nltk.stem.wordnet import WordNetLemmatizer
-# from spacy.lang.en import English
+from rdflib import URIRef, Literal, Namespace
+from rdflib.namespace import RDF, OWL
 import spacy
-from svo_extractor import findSVOs, findSVAOs
-
-# from nltk.parse import CoreNLPParser
-# from nltk.tree import ParentedTree
-
 from pycorenlp import StanfordCoreNLP
 
-# parser = CoreNLPParser(url='http://localhost:9000')
-# nlp = StanfordCoreNLP('http://localhost:9000')
-nlp = spacy.load('en')
+SCHEMA = Namespace("https://schema.org/")
+
+stanford_nlp = StanfordCoreNLP('http://localhost:9000')
+spacy_nlp = spacy.load('en')
 
 only_place_filter = {'policy': "whitelist", 'types': "DBpedia:Place", 'coreferenceResolution': False}
-only_year_filter = {'policy': "whitelist", 'types': "DBpedia:Year", 'coreferenceResolution': False}
+only_person_filter = {'policy': "whitelist", 'types': "DBpedia:Person", 'coreferenceResolution': False}
+only_organization_filter = {'policy': "whitelist", 'types': "DBpedia:Organisation", 'coreferenceResolution': False}
+
+currency_regex = "(?:[a-zA-Z]*[\£\$\€]{1}[,\d]+)"
+US_dollars_regex = "(?:[,\d]+.U.S. dollars)"
+euros_regex = "(?:[,\d]+.euros)"
+CUR_regex = "(?:[,\d]+.[A-Z][A-Z][A-Z])"
+francs_regex = "(?:[,\d]+.[a-z]*.francs)"
 
 
+# extract the money related instances
+def get_money_instances(sentence):
+    money = re.findall(currency_regex, sentence)
+    money = set(money)
 
-def UBI_process_content(content):
-    sentiment = None
+    us_dollars = re.findall(US_dollars_regex, sentence)
+    if len(us_dollars) > 0:
+        for us_dollar in us_dollars:
+            money.add("US$" + re.split(" ", us_dollar)[0])
+
+    euros = re.findall(euros_regex, sentence)
+    if len(euros) > 0:
+        for euro in euros:
+            money.add("€" + re.split(" ", euro)[0])
+
+    curs = re.findall(CUR_regex, sentence)
+    if len(curs) > 0:
+        for cur in curs:
+            money.add(cur)
+
+    francs = re.findall(francs_regex, sentence)
+    if len(francs) > 0:
+        for franc in francs:
+            money.add(franc)
+
+    return money
+
+
+# extract the date related instances
+def get_date_instances(annotated_sentence):
+
+    dates = set()
+
+    for annotated_word in annotated_sentence["sentences"][0]["tokens"]:
+        if annotated_word["ner"] == "DATE":
+            dates.add(annotated_word["normalizedNER"])
+
+    return dates
+
+
+# extract the location related instances
+def get_location_instances(annotated_sentence):
+
     places = set()
-    year = set()
-    plan = set()
-    economy = set()
-    positives = set()
-    negatives = set()
 
+    for annotated_word in annotated_sentence["sentences"][0]["tokens"]:
+        if annotated_word["ner"] == "LOCATION" or annotated_word["ner"] == "COUNTRY" or annotated_word["ner"] == "CITY":
+            if annotated_word["originalText"] != annotated_word["lemma"]:
+                continue
+            else:
+                places.add(annotated_word["word"])
+
+    return places
+
+
+# extract the person related instances
+def get_person_instances(annotated_sentence):
+
+    people = set()
+    person = []
+
+    for annotated_word in annotated_sentence["sentences"][0]["tokens"]:
+        if annotated_word["ner"] == "PERSON":
+            person.append(annotated_word["word"])
+            continue
+        elif len(person) != 0:
+            people.add(" ".join(person))
+            person.clear()
+
+    return people
+
+
+# extract the organization related instances
+def get_organization_instances(annotated_sentence):
+
+    organizations = set()
+    organization = []
+
+    for annotated_word in annotated_sentence["sentences"][0]["tokens"]:
+        if annotated_word["ner"] == "ORGANIZATION":
+            organization.append(annotated_word["word"])
+            continue
+        elif len(organization) != 0:
+            organizations.add(" ".join(organization))
+            organization.clear()
+
+    return organizations
+
+
+# process sentences to extract useful information (money, dates, places, people, organizations)
+# from the content passed as argument
+def extract_info(content):
+    processed_sentences = []
     sentences = nltk.sent_tokenize(content)
 
     for sentence in sentences:
-        # print(sentence)
+        stanford_annontation = stanford_nlp.annotate(sentence, properties={'annotators': 'ner',
+                                                                           'outputFormat': 'json',
+                                                                           'timeout': 5000,
+                                                                           })
+        money = get_money_instances(sentence)
+        dates = get_date_instances(stanford_annontation)
+        places = get_location_instances(stanford_annontation)
+        people = get_person_instances(stanford_annontation)
+        organizations = get_organization_instances(stanford_annontation)
 
-        # try:
-        #     annotations = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate', sentence)
-        #     # print(annotations)
-        # except:
-        #     continue
+        # print(money)
+        # print(dates)
+        # print(places)
+        # print(people)
+        # print(organizations)
 
+        processed_sentences.append((money, dates, places, people, organizations))
+
+    return processed_sentences
+
+
+# link to dbpedia instance linking
+def instance_linking(info_tuple):
+
+    place_annotations = {}
+    people_annotations = {}
+    organization_annotations = {}
+
+    _, _, places, people, organizations = info_tuple
+
+    for place in places:
         try:
-            year_annotations = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate', sentence,
-                                                  filters=only_year_filter)
-            for year_info in year_annotations:
-                year.add(year_info['URI'])
+            place_annotations[place] = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate',
+                                                          place, filters=only_place_filter)[0]['URI']
         except:
-            pass
+            continue
 
+    for person in people:
         try:
-            place_annotations = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate', sentence,
-                                                   filters=only_place_filter)
-            for place_info in place_annotations:
-                places.add(place_info['URI'])
+            people_annotations[person] = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate',
+                                                            person, filters=only_person_filter)[0]['URI']
         except:
-            pass
+            continue
 
-        # parse = nlp("Donald Trump is the worst president of USA, but Hillary is better than him")
-        # print(findSVAOs(nlp(sentence)))
+    for organization in organizations:
+        try:
+            organization_annotations[organization] = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate',
+                                                                        organization,
+                                                                        filters=only_organization_filter)[0]['URI']
+        except:
+            continue
 
-    return sentiment, places, year, plan, economy, positives, negatives
+    return place_annotations, people_annotations, organization_annotations
 
 
+def add_instances(graph, place, project, sentences_tuple):
+    money, dates, locations, people, organizations = sentences_tuple
+    place_annotations, people_annotations, organization_annotations = instance_linking(sentences_tuple)
 
+    if len(money) != 0:
+        for amount in money:
+            graph.add((project["place/" + place.replace(" ", "_")], project.associated_scheme, Literal(amount)))
+
+    if len(dates) != 0:
+        for date in dates:
+            graph.add((project["place/" + place.replace(" ", "_")], project.associated_time, Literal(date)))
+
+    if len(locations) != 0:
+        for location in locations:
+            graph.add((project["place/" + location.replace(" ", "_")], RDF.type, SCHEMA.Place))
+            graph.add((project["place/" + place.replace(" ", "_")], project.associated_place,
+                       project["place/" + location.replace(" ", "_")]))
+            graph.add((project["place/" + place.replace(" ", "_")], SCHEMA.containsPlace,
+                       project["place/" + location.replace(" ", "_")]))
+            if location in place_annotations:
+                graph.add((project["place/" + location.replace(" ", "_")], OWL.sameAs,
+                           URIRef(place_annotations[location])))
+
+    if len(people) != 0:
+        for person in people:
+            graph.add((project["person/" + person.replace(" ", "_")], RDF.type, SCHEMA.Person))
+            graph.add((project["place/" + place.replace(" ", "_")], project.associated_person,
+                       project["person/" + person.replace(" ", "_")]))
+            if person in people_annotations:
+                graph.add((project["person/" + person.replace(" ", "_")], OWL.sameAs,
+                           URIRef(people_annotations[person])))
+
+    if len(organizations) != 0:
+        for organization in organizations:
+            graph.add((project["organization/" + organization.replace(" ", "_")], RDF.type, SCHEMA.Organization))
+            graph.add((project["place/" + place.replace(" ", "_")], project.associated_organization,
+                       project["organization/" + organization.replace(" ", "_")]))
+            if organization in organization_annotations:
+                graph.add((project["organization/" + organization.replace(" ", "_")], OWL.sameAs,
+                           URIRef(organization_annotations[organization])))
+
+    if len(locations) != 0 and len(dates) != 0 and len(money) != 0:
+        for location in locations:
+            for date in dates:
+                graph.add((project["place/" + location.replace(" ", "_")], project.associated_time, Literal(date)))
+                for amount in money:
+                    graph.add((project["place/" + location.replace(" ", "_")],
+                               project.associated_scheme, Literal(amount)))
+    elif len(locations) != 0 and len(dates) != 0:
+        for location in locations:
+            for date in dates:
+                graph.add((project["place/" + location.replace(" ", "_")], project.associated_time, Literal(date)))
+    elif len(locations) != 0 and len(money) != 0:
+        for location in locations:
+            for amount in money:
+                graph.add((project["place/" + location.replace(" ", "_")], project.associated_scheme, Literal(amount)))
+
+# content = "Starting in 2014, the Liberal Party of Canada,[119] the Green Party of Canada,[120] the Pirate Party of Canada,[121] provincial party Québec Solidaire,[122] and conservative senator Hugh Segal[123] have been advocating for basic income in Canada"
+# processed_sentences = extract_info(content)
+# place_annotations, people_annotations, organization_annotations = instance_linking(processed_sentences[0])
+# print('here')
--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from wiki_extractor import WikiExtractor
-from content_processing import UBI_process_content
+from content_processing import extract_info, add_instances
 
 import spotlight
 
 from rdflib import Graph
-from rdflib import URIRef, BNode, Literal
-from rdflib.namespace import RDF, FOAF
+from rdflib import URIRef
+from rdflib.namespace import RDF, OWL
 from rdflib import Namespace
 
-from nltk.parse import CoreNLPParser
-from nltk.tree import ParentedTree, Tree
-
-# parser = CoreNLPParser(url='http://localhost:9000')
-
-PROJECT = Namespace("https://UBI_project.org/")
+PROJECT = Namespace("https://UBI_project.org/UBI_wiki/")
 SCHEMA = Namespace("https://schema.org/")
 graph = Graph()
 
@@ -31,58 +26,59 @@ only_place_filter = {'policy': "whitelist", 'types': "DBpedia:Place", 'coreferen
 for section in all_sections:
     if isinstance(section, dict):
         for key in section.keys():
+            place = key
+            graph.add((PROJECT[place.replace(" ", "_")], RDF.type, SCHEMA.Place))
             try:
-                section_annotations = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate', key,
+                section_annotations = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate', place,
                                                          confidence=0.5, filters=only_place_filter)
-                # print(section_annotations)
-                graph.add((URIRef(section_annotations[0]['URI']), RDF.type, SCHEMA.Place))
-                for subsection in section[key]:
-                    if isinstance(subsection, dict):
-                        try:
-                            subsection_titles.append(list(subsection.keys())[0])
-                            subsection_annotations = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate',
-                                                                        list(subsection.keys())[0], confidence=0.5,
-                                                                        filters=only_place_filter)
-                        except:
-                            continue
-                    else:
-                        try:
-                            subsection_annotations = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate',
-                                                                        subsection, confidence=0.5,
-                                                                        filters=only_place_filter)
-                            content = wiki_extract.get_section_content(page_title=page_title, section_title=subsection)
-                            sentiment, places, years, plan, economy, positives, negatives = UBI_process_content(content)
-                        except:
-                            continue
-                    # print(subsection_annotations)
-                    graph.add((URIRef(subsection_annotations[0]['URI']), RDF.type, SCHEMA.Place))
-                    if len(places) != 0:
-                        for place in places:
-                            graph.add((URIRef(subsection_annotations[0]['URI']), PROJECT.implemented_at, URIRef(place)))
-                    if len(years) != 0:
-                        for year in years:
-                            graph.add((URIRef(subsection_annotations[0]['URI']), PROJECT.associated_year, URIRef(year)))
+                graph.add((URIRef(section_annotations[0]['URI']), OWL.sameAs, PROJECT["place/" + place.replace(" ", "_")]))
             except:
-                continue
+                print("")
+
+            for subsection in section[key]:
+                if isinstance(subsection, dict):
+                    place = list(subsection.keys())[0]
+                else:
+                    place = subsection
+
+                graph.add((PROJECT["place/" + place.replace(" ", "_")], RDF.type, SCHEMA.Place))
+                graph.add(
+                    (PROJECT["place/" + key.replace(" ", "_")], SCHEMA.containsPlace,
+                     PROJECT["place/" + place.replace(" ", "_")]))
+                try:
+                    subsection_annotations = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate',
+                                                                place, confidence=0.5,
+                                                                filters=only_place_filter)
+                    graph.add((URIRef(subsection_annotations[0]['URI']), OWL.sameAs,
+                               PROJECT["place/" + place.replace(" ", "_")]))
+                except:
+                    print("")
+
+                if isinstance(subsection, dict):
+                    for subsubsection in list(subsection.values())[0]:
+                        try:
+                            subsubsection_annotations = \
+                                spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate',
+                                                   subsubsection, confidence=0.5, filters=only_place_filter)
+                            place = subsubsection_annotations[0]['surfaceForm']
+                            graph.add((PROJECT["place/" + place.replace(" ", "_")], RDF.type, SCHEMA.Place))
+                            graph.add((PROJECT["place/" + key.replace(" ", "_")],
+                                       SCHEMA.containsPlace, PROJECT["place/" + place.replace(" ", "_")]))
+                            graph.add((URIRef(subsubsection_annotations[0]['URI']),
+                                       OWL.sameAs, PROJECT["place/" + place.replace(" ", "_")]))
+                        except:
+                            print("")
+
+                        content = wiki_extract.get_section_content(page_title=page_title, section_title=subsubsection)
+                        processed_sentences = extract_info(content)
+
+                        for sentences_tuple in processed_sentences:
+                            add_instances(graph, place, PROJECT, sentences_tuple)
+                else:
+                    content = wiki_extract.get_section_content(page_title=page_title, section_title=subsection)
+                    processed_sentences = extract_info(content)
+
+                    for sentences_tuple in processed_sentences:
+                        add_instances(graph, place, PROJECT, sentences_tuple)
 
 print(graph.serialize(destination='wiki_UBI.nt', format='nt'))
-
-
-# annotations = spotlight.annotate('http://api.dbpedia-spotlight.org/en/annotate',
-#                                  "January")
-# print(annotations[0]["types"].split(","))
-
-
-
-# sent = 'After the launch, the project was found to have significantly reduced child malnutrition and increased school attendance'
-# t = list(parser.raw_parse(sent))[0]
-# t = ParentedTree.convert(t)
-#
-# t.pretty_print()
-#
-#
-#
-#
-# print(find_subject(t))
-# print(find_predicate(t))
-# print(find_object(t))
